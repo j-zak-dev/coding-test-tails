ba60eb9c092b1725726e03181c4dc55c35292744
Jakub Zak <jakub.zak@tails.com>
2026-04-12 08:30:52 +0100

    add valid dto mapping for application

--- src/backend/application/services/store_service.py
+++ src/backend/application/services/store_service.py
@@ -1,3 +1,4 @@
+from application.mappers.to_dto.DTOStoreMapper import DTOStoreMapper
 from domain.interfaces.storeInterface import StoreInterface
 
 
@@ -6,10 +7,13 @@ class StoreService:
         self.store_repository = store_repository
 
     def get_all_stores(self):
-        return self.store_repository.get_all_stores()
+        stores = self.store_repository.get_all_stores()
+        return [DTOStoreMapper.to_store_response_dto(store) for store in stores]
 
     def search_stores_by_name(self, name):
-        return self.store_repository.search_stores_by_name(name)
+        stores = self.store_repository.search_stores_by_name(name)
+        return [DTOStoreMapper.to_store_response_dto(store) for store in stores]
 
     def search_store_by_postcode(self, postcode):
-        return self.store_repository.search_store_by_postcode(postcode)
+        stores = self.store_repository.search_store_by_postcode(postcode)
+        return [DTOStoreMapper.to_store_response_dto(store) for store in stores]
